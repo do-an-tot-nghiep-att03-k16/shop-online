df5caade824dcd4f21c46b4a63411571610b90b8
Thanh Van Nguyen <qingyunruan1605@gmail.com>
2026-01-02 09:43:00 +0000

    fix: default config cms https

--- frontend-clothing-shop/src/config/env.js
+++ frontend-clothing-shop/src/config/env.js
@@ -1,14 +1,16 @@
 const API_BASE_URL =
     import.meta.env.VITE_API_BASE_URL || 'http://localhost:3055/v1/api'
 const API_STRAPI_URL =
-    import.meta.env.VITE_API_STRAPI_URL || 'http://localhost:1337/api'
+    import.meta.env.VITE_API_STRAPI_URL || 'https://cms.aristia.shop'
 
 // Supabase Configuration
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || ''
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY || ''
 
-// N8N Chatbot Configuration  
-const N8N_WEBHOOK_URL = import.meta.env.VITE_N8N_WEBHOOK_URL || 'http://localhost:5678/webhook-test/chatbot'
+// N8N Chatbot Configuration
+const N8N_WEBHOOK_URL =
+    import.meta.env.VITE_N8N_WEBHOOK_URL ||
+    'http://localhost:5678/webhook-test/chatbot'
 
 export const envConfig = {
     API_BASE_URL,
